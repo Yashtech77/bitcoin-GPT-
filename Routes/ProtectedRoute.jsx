758d2d7a0e5828e7010c105409aa45afa8357716
yashraj7781 <yashrajdhoni151@gmail.com>
2025-08-18 16:12:21 +0530

    test text vanish after hitting enter , and logout funtion after local sotrage got clear

--- Routes/ProtectedRoute.jsx
+++ Routes/ProtectedRoute.jsx
@@ -1,13 +1,27 @@
-
 import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
   if (!token) {
-
     return <Navigate to="/login" replace />;
+  }
 
+  try {
+    const decoded = jwtDecode(token);
+    const currentTime = Date.now() / 1000; // current time in seconds
+
+    if (decoded.exp && decoded.exp < currentTime) {
+      // Token expired
+      localStorage.removeItem("token");
+      return <Navigate to="/login" replace />;
+    }
+  } catch (error) {
+    // Invalid token format
+    localStorage.removeItem("token");
+    return <Navigate to="/login" replace />;
   }
 
   return children;
